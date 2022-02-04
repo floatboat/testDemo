310ecdef11747aade2445a231ad6d9dbba8c274b
floatboat <thuan.do2003@yahoo.com>
2022-02-04 11:24:11 -0800

    change

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -3,6 +3,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test
     public void mulitply() {
-        assertEquals(2, testSkillDemo.multiply(1,3));
+        assertEquals(3, testSkillDemo.multiply(1,3));
     }
 }
